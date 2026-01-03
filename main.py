f387ad5e1a38937945fef98252a7f953d9f46d4d
Farell-123 <farell95130@gmail.com>
2026-01-03 10:27:55 +0100

    Ajout analyse de rentabilité et scoring

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-#Ce script analyse la rentabilité de produits à partir d’un fichier CSV
+## Description
+#This project analyzes resale profitability for sellers.
+
+## Description (FR)
+#Ce projet analyse la rentabilité de produits pour la revente.
+
 import csv
 
 with open("data/products.csv", newline="", encoding="utf-8") as file:
     reader = csv.DictReader(file)
     for row in reader:
         #print(row)
-        achat=float(row["prix_achat"])
-        vente=float(row["prix_vente"])
-        benef=vente-achat
-        marge=(benef/achat)*100
-        if benef >= 10:
+        buy=float(row["buy_price"])
+        sell=float(row["sell_price"])
+        profit=sell-buy
+        margin=(profit/buy)*100
+        if margin >= 100:
                 status = "🟢 EXCELLENT"
-        if benef >= 5 and benef<10:
+        elif margin >= 50 and margin<100:
                 status = "🟡 CORRECT"
-        if benef<5:
-                status = "🔴 MAUVAIS"
-        print(f"{row['nom']} | Profit: {benef}€ | Marge: {marge:.1f}% | {status}")
+        else:
+             status = "🔴 MAUVAIS"
+        print(f"{row['name']} | Profit: {profit}€ | Marge: {margin:.1f}% | {status}")
+
+
+
